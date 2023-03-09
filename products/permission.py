8de5bc78140580c48aa2f808ec6a64548d551f51
davi894 <moisesdavi005@gmail.com>
2023-03-08 23:59:38 -0300

    remove: removendo ipdb.set_trace

--- products/permission.py
+++ products/permission.py
@@ -5,8 +5,6 @@ import ipdb
 class CreateProductPermission(permissions.BasePermission):
     def has_permission(self, request, view):
 
-        # ipdb.set_trace()
-
         return (
             request.user.is_superuser or request.user.employee
         ) and request.user.is_authenticated
